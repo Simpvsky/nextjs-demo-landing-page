e9a7225adef9f731e7eca0cf9e7393b333d564a0
Simpvsky <147368392+Simpvsky@users.noreply.github.com>
2025-05-29 19:07:44 -0600

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1748567262964

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_9dc28c78._.js",
+  "static/chunks/src_9ee8901d._.js"
+],
+    source: "dynamic"
+});
